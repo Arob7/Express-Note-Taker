31214ecbe857ffb524aa4a3bf7c92f6144f8965b
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-08-25 19:22:06 -0400

    created HTML routes

--- server.js
+++ server.js
@@ -5,3 +5,13 @@ const fs = require("fs");
 const app = express();
 const PORT = 8080;
 const mainDir = path.join(__dirname, "/public");
+
+// RETURNS NOTES HTML FILE
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(mainDir, "notes.html"));
+});
+
+// RETURNS INDEX HTML FILE
+app.get("*", function (req, res) {
+  res.sendFile(path.join(mainDir, "index.html"));
+});
