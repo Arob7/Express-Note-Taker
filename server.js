094c2666ba89602bff82874d0eeeb82f072d2771
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-08-25 21:02:27 -0400

    added inital port, data parsing and routes folder with apiRoutes and htmlRoutes

--- server.js
+++ server.js
@@ -3,15 +3,21 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = 8080;
+
+// SETS INITIAL PORT
+const PORT = process.env.PORT || 3000;
 const mainDir = path.join(__dirname, "/public");
 
+// DATA PARSING
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // RETURNS NOTES HTML FILE
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(mainDir, "notes.html"));
-});
+// app.get("/notes", function (req, res) {
+//   res.sendFile(path.join(mainDir, "notes.html"));
+// });
 
 // RETURNS INDEX HTML FILE
-app.get("*", function (req, res) {
-  res.sendFile(path.join(mainDir, "index.html"));
-});
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(mainDir, "index.html"));
+// });
