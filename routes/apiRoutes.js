f452145915f8ac8ad407685b41391609f3dc20ff
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-08-28 16:24:19 -0400

    changed app to router on apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,20 +12,32 @@ router.get("/notes", function (req, res) {
     if (error) {
       return console.log(error);
     }
-    console.log(data);
-    console.log(typeof data);
-
     res.json(JSON.parse(data));
   });
 });
 
 // SEND REQUEST TO DATABASE AND GET RESPONSE HERE
-router.post("path", function (req, res) {
+router.post("/notes", function (req, res) {
   const request = req.body;
+  res.send;
 });
 
 // * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 
 // * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+router.delete("/api/notes/:id", function (req, res) {
+  const id = req.params.id;
+  for (let note of db) {
+    if (id === note.id) {
+      const noteIndex = db.indexOf(note);
+      db.splice(noteIndex, 1);
+      fs.writeFile(filePath, JSON.stringify(db), (err) => {
+        if (err) throw err;
+        console.log("Note deleted");
+      });
+      res.end();
+    }
+  }
+});
 
 module.exports = router;
